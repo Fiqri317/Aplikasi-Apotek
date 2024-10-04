2f83d4f122b44df86d75312496a60beb82e86969
Fiqri317 <139039571+Fiqri317@users.noreply.github.com>
2024-10-04 18:14:28 +0700

    main

---
+++ src/Aplikasi Apotek/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dgv_coba = New System.Windows.Forms.DataGridView()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Bs_coba = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.dgv_coba, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Bs_coba, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Monotype Corsiva", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(63, 52)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(331, 33)
+        Me.Label9.TabIndex = 41
+        Me.Label9.Text = "Sistem Informasi Penjualan Obat"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(61, 3)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(459, 55)
+        Me.Label8.TabIndex = 40
+        Me.Label8.Text = "APLIKASI APOTEK"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(145, 358)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 39
+        Me.Button5.Text = "Kembali"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(64, 358)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 38
+        Me.Button4.Text = "Refresh"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(174, 323)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 37
+        Me.Button3.Text = "Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 323)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 36
+        Me.Button2.Text = "Hapus"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 323)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 35
+        Me.Button1.Text = "Simpan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'dgv_coba
+        '
+        Me.dgv_coba.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_coba.Location = New System.Drawing.Point(257, 199)
+        Me.dgv_coba.Name = "dgv_coba"
+        Me.dgv_coba.Size = New System.Drawing.Size(542, 186)
+        Me.dgv_coba.TabIndex = 34
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(343, 163)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(211, 20)
+        Me.TextBox5.TabIndex = 33
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(254, 166)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 13)
+        Me.Label7.TabIndex = 32
+        Me.Label7.Text = "Cari ID Pegawai"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Laki-Laki", "Perempuan"})
+        Me.ComboBox1.Location = New System.Drawing.Point(112, 260)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 31
+        Me.ComboBox1.Text = "Pilih"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(112, 291)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox4.TabIndex = 30
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(112, 228)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox3.TabIndex = 29
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(112, 196)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox2.TabIndex = 28
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(112, 163)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox1.TabIndex = 27
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 294)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 26
+        Me.Label6.Text = "Jabatan"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 263)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "Jenis Kelamin"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 231)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Alamat"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 199)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 23
+        Me.Label3.Text = "Nama Pegawai"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 166)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "ID Pegawai"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(318, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 31)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "Data Pegawai"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Aplikasi_Apotek.My.Resources.Resources.images__1__removebg_preview
+        Me.PictureBox1.Location = New System.Drawing.Point(-12, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 86)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 42
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(806, 403)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.dgv_coba)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgv_coba, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Bs_coba, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents dgv_coba As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Bs_coba As System.Windows.Forms.BindingSource
+
+End Class
---
+++ src/Aplikasi Apotek/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.OleDb
+Public Class Form1
+    Dim _koneksiString As String
+    Dim _koneksi As New OleDbConnection
+    Dim komandambil As New OleDbCommand
+    Dim datatabelku As New DataTable
+    Dim dataadapterku As New OleDbDataAdapter
+    Dim x As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        _koneksiString = "Provider=Microsoft.Jet.OleDb.4.0;" + "Data Source=D:\Campus\Semester V\Kecerdasan Komputasi\Aplikasi-Apotek\database\Aplikasi Apotek.mdb;"
+        _koneksi.ConnectionString = _koneksiString
+        _koneksi.Open()
+
+        komandambil.Connection = _koneksi
+        komandambil.CommandType = CommandType.Text
+
+        komandambil.CommandText = "SELECT * FROM Pegawai"
+        dataadapterku.SelectCommand = komandambil
+        dataadapterku.Fill(datatabelku)
+        Bs_coba.DataSource = datatabelku
+        dgv_coba.DataSource = Bs_coba
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim cmdTambah As New OleDbCommand
+        Dim tanya As String
+        Dim x As DataRow
+        cmdTambah.Connection = _koneksi
+        cmdTambah.CommandText = "INSERT INTO " + "Pegawai ([ID Pegawai], [Nama Pegawai], Alamat, [Jenis Kelamin], Jabatan)" + "VALUES ('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + ComboBox1.Text + "','" + TextBox4.Text + " ')"
+        tanya = MessageBox.Show("Data Ini di Simpan ?", "info", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If tanya = vbYes Then
+            cmdTambah.ExecuteNonQuery()
+            x = datatabelku.NewRow
+            x("ID Pegawai") = TextBox1.Text
+            x("Nama Pegawai") = TextBox2.Text
+            x("Alamat") = TextBox3.Text
+            x("Jenis Kelamin") = ComboBox1.Text
+            x("Jabatan") = TextBox4.Text
+            datatabelku.Rows.Add(x)
+            Bs_coba.DataSource = Nothing
+            Bs_coba.DataSource = datatabelku
+
+            dgv_coba.Refresh()
+            Bs_coba.MoveFirst()
+        End If
+    End Sub
+
+    Private Sub dgv_coba_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgv_coba.CellContentClick
+        TextBox1.Text = dgv_coba.CurrentRow.Cells(0).Value.ToString
+        TextBox2.Text = dgv_coba.CurrentRow.Cells(1).Value.ToString
+        TextBox3.Text = dgv_coba.CurrentRow.Cells(2).Value.ToString 
+        ComboBox1.Text = dgv_coba.CurrentRow.Cells(3).Value.ToString
+        TextBox4.Text = dgv_coba.CurrentRow.Cells(4).Value.ToString
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim cmdHapus As New OleDbCommand
+        cmdHapus.Connection = _koneksi
+        cmdHapus.CommandType = CommandType.Text
+        x = MessageBox.Show("Yakin Data Akan di Hapus ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If x = vbYes Then
+            cmdHapus.CommandText = "DELETE FROM " + "Pegawai WHERE [ID Pegawai]=" + TextBox1.Text
+            cmdHapus.ExecuteNonQuery()
+        End If
+        Bs_coba.RemoveCurrent()
+        dgv_coba.Refresh()
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        ComboBox1.Items.Clear()
+        TextBox4.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim cmdUpdate As New OleDbCommand
+        cmdUpdate.Connection = _koneksi
+        cmdUpdate.CommandType = CommandType.Text
+        x = MessageBox.Show("Yakin Data Ingin di Perbarui?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If x = vbYes Then
+            cmdUpdate.CommandText = "UPDATE Pegawai SET " +
+                "[Nama Pegawai] = '" + TextBox2.Text + "', " +
+                "Alamat = '" + TextBox3.Text + "', " +
+                "[Jenis Kelamin] = '" + ComboBox1.Text + "', " +
+                "Jabatan = '" + TextBox4.Text + "' " +
+                "WHERE [ID Pegawai] = " + TextBox1.Text  '
+            cmdUpdate.ExecuteNonQuery()
+            Dim rowToUpdate As DataRow = datatabelku.Select("[ID Pegawai] = " + TextBox1.Text).FirstOrDefault()
+            If rowToUpdate IsNot Nothing Then
+                rowToUpdate("ID Pegawai") = TextBox1.Text
+                rowToUpdate("Nama Pegawai") = TextBox2.Text
+                rowToUpdate("Alamat") = TextBox3.Text
+                rowToUpdate("Jenis Kelamin") = ComboBox1.Text
+                rowToUpdate("Jabatan") = TextBox4.Text
+            End If
+            Bs_coba.DataSource = Nothing
+            Bs_coba.DataSource = datatabelku
+            dgv_coba.Refresh()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        ComboBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox5.TextChanged
+        datatabelku.Clear()
+
+        komandambil.Connection = _koneksi
+        komandambil.CommandType = CommandType.Text
+        komandambil.CommandText = "SELECT * FROM Pegawai WHERE [ID Pegawai] LIKE '%" + TextBox5.Text + "%' " +
+                                       "OR [Nama Pegawai] LIKE '%" + TextBox5.Text + "%' " +
+                                       "OR [Alamat] LIKE '%" + TextBox5.Text + "%' " +
+                                       "OR [Jenis Kelamin] LIKE '%" + TextBox5.Text + "%' " +
+                                       "OR [Jabatan] LIKE '%" + TextBox5.Text + "%'"
+
+        dataadapterku.SelectCommand = komandambil
+        dataadapterku.Fill(datatabelku)
+
+        Bs_coba.DataSource = datatabelku
+        dgv_coba.DataSource = Bs_coba
+        dgv_coba.Refresh()
+    End Sub
+End Class
